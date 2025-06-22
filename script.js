5cdb92809910e1631064825cc64557daa68cb599
Leonardo Paciello Ferreira Dias <leonardopaciellop@gmail.com>
2025-06-22 16:21:55 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,5 @@
+function messageClick() {
+  const messageElement = document.getElementById("message");
+  messageElement.textContent =
+    "Olá, Seja bem-vindo. Este é o meu projeto realizado com HTML, CSS e JavaScript!";
+}
